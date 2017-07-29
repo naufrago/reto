49addebdd1d4021f222ad34487de37f6e535f9c9
yeison aguirre osorio <yaguirreo@unal.edu.co>
2017-07-29 09:58:27 -0500

    recambio

--- conexion.php
+++ conexion.php
@@ -1,14 +1,11 @@
 <?php
-	/*datos necesarios para la conexion
+	//datos necesarios para la conexion
 	$db_host="localhost";//direccion del serviedor
 	$db_nombre="bd_reto";//nombre de la BD
 	$db_usuario="root";//"root" por defecto en local
-	$db_contra="";//en local es vacia por defecto*/
+	$db_contra="";//en local es vacia por defecto
 
-	$db_host="mysql.webcindario.com";//direccion del serviedor
-	$db_nombre="securitychall";//nombre de la BD
-	$db_usuario="securitychall";//"root" por defecto en local
-	$db_contra="securitychallreto123";//en local es vacia por defecto
+	
 
 	//sentecia para conectar la BDD a la WEB
 	$conexion=mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre);
